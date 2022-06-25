bd4d1884a63842b9e354ce29401172fb162b6bb7
Marc Servat <marc.servat@sage.com>
2022-06-25 11:03:10 +0200

    fix: reformat code

--- beer-search/src/components/Button/Button.tsx
+++ beer-search/src/components/Button/Button.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./Button.css";
 
 interface ButtonProps {
-    content: string;
-    onClick: () => void;
+  content: string;
+  onClick: () => void;
 }
 
 const Button = (props: ButtonProps) => {
-    return (
-        <button className="button-component" onClick={props.onClick}>{props.content}</button>
-    )
-}
+  return (
+    <button className="button-component" onClick={props.onClick}>
+      {props.content}
+    </button>
+  );
+};
 
 export default Button;
